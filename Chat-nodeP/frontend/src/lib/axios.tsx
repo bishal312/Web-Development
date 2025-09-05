0d47aa0bbec8af2ff48c913e5199f65095fb10eb
Bsal <bishalm626@gmail.com>
2025-09-05 20:19:13 +0545

    Deployed..

--- Chat-nodeP/frontend/src/lib/axios.tsx
+++ Chat-nodeP/frontend/src/lib/axios.tsx
@@ -1,6 +1,11 @@
 import axios from "axios";
 
+// export const axiosInstance = axios.create({
+//   baseURL: "http://localhost:5000/api",
+//   withCredentials: true
+// })
+
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
-  withCredentials: true
-})
+  baseURL: "https://nepalipool.onrender.com", // <-- your deployed backend
+  withCredentials: true, // if using cookies / sessions
+});
