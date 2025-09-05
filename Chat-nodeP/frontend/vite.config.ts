2111b9165f26f7194b7f9de4770b4470a66a5579
Bsal <bishalm626@gmail.com>
2025-09-05 19:32:42 +0545

    Deploy

--- Chat-nodeP/frontend/vite.config.ts
+++ Chat-nodeP/frontend/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   build: {
   rollupOptions: {
     output: {
-      manualChunks(id) {
+      manualChunks(id:string) {
         if (id.includes("node_modules")) {
           return "vendor";
         }
